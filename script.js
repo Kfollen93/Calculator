c982b5b19216c1f61f35feb6753aba053c893417
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-03-17 19:10:24 -0700

    Updated addition function to work with the DOM

--- script.js
+++ script.js
@@ -1,33 +1,123 @@
-function add(...theArgs) {
-    const sum = theArgs.reduce((acc, arg) => acc + arg, 0);
-    return sum;
-    
-  }
+/*function add(a, b)
+{
+  return a + b;
+}
+const additionButton = document.querySelector('#plusButton');
+additionButton.addEventListener('click', () =>
+{
+  add();
+});
 
-  function subtract(...theArgs) {
-    const sum = theArgs.reduce((acc, arg) => acc - arg);
-    return sum;
-    
-  }
-
-  function multiply(...theArgs) {
-    const sum = theArgs.reduce((acc, arg) => acc * arg);
-    return sum;
-    
-  }
-
-  function divide(...theArgs) {
-    const sum = theArgs.reduce((acc, arg) => acc / arg);
-    return sum;
-    
-  }
-
-  function operate()
+function subtract(a, b)
+{
+  return a - b;
+}
+function multiply(a, b)
+{
+  return a * b;
+}
+function divide(a, b)
+{
+  if (a / b === Infinity || a / b === -Infinity || a === 0 && b === 0)
   {
-      //takes an operator and 2 numbers and then calls one of the above functions above on the numbers
+    return errorMessage;
+  } 
+  else
+  {
+    return a / b;
+  }
+ 
+}
+const errorMessage = "Can not do"; */
+
+let num1;
+let num1parsed;
+let num2;
+let num2parsed;
+let operation;
+
+function add()
+{
+  num1 = document.getElementById('display').textContent;
+  num1parsed = parseInt(num1);
+  console.log(num1parsed);
+  operation = "add";
+  document.getElementById('display').innerHTML = '';
+}
+document.getElementById('plusButton').addEventListener('click', add);
+
+
+function operate(operation, num1, num2)
+{
+    switch(operation) {
+        case "add": return num1 + num2;
+        case "subtract": return num1 - num2;
+        case "multiply": return num1 * num2;
+        case "divide": return num1 / num2;
+    };
+}
+
+const equalButton = document.querySelector('#equalButton');
+equalButton.addEventListener('click', () =>
+{
+  num2 = document.getElementById('display').textContent;
+  num2parsed = parseInt(num2);
+  console.log(num2parsed);
+  document.getElementById('display').innerHTML = operate(operation, num1parsed, num2parsed);
+  console.log(num1parsed + num2parsed);
+});
+//-------------------------------------------------------------------------
+//Functions that populate the display when you click a number
+
+  function showSeven()
+  {
+    document.getElementById('display').style.color = 'white';
+    document.getElementById('display').innerHTML += 7;
   }
 
+  const sevenButton = document.querySelector('#sevenButton');
+  sevenButton.addEventListener('click', () =>
+  {
+    showSeven();
+  });
+
+  function showEight()
+  {
+    document.getElementById('display').style.color = 'white';
+    document.getElementById('display').innerHTML += 8;
+  }
+
+  const eightButton = document.querySelector('#eightButton');
+  eightButton.addEventListener('click', () =>
+  {
+    showEight();
+  });
 
 
+  //clear function --------------------------------------------------------
+  function allClear()
+  {
+    document.getElementById('display').innerHTML = '';
+  }
 
+  const allClearButton = document.querySelector('#clearButton');
+  clearButton.addEventListener('click', () =>
+  {
+    allClear();
+  });
 
+ //function for all digit values------------------------------------
+
+ /*const digits = document.getElementsByClassName("digit");
+ digits.forEach(function(digit){
+   digit.addEventListener("click", function(eventObject){
+     // in here, when a button is clicked, we can see which one by
+     //  that eventObject - it has a 'target' property which is the
+     //  element that caused the event!
+     const value = eventObject.target.textContent;
+     // in your case, the buttons don't have a value, but their
+     //  actual text is pretty much what we want.
+   })
+ }) */ //digits[I].addEventListener
+
+ //Do a foor loop for each button to make it display
