8f80a3f6f7c9bed167c791f75e5c52128f9deccf
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-03-20 18:29:49 -0700

    Added decimal function

--- script.js
+++ script.js
@@ -1,59 +1,71 @@
 const errorMessage = "Can not do";
-let num1;
-let num1parsed;
-let num2;
-let num2parsed;
+let num1 = 0.0;
+let num1parsed = 0.0;
+let num2 = 0.0;
+let num2parsed = 0.0;
 let operation;
 
+//All operator functions ---------------------------
 function add()
 {
   num1 = document.getElementById('display').textContent;
-  num1parsed = parseInt(num1);
-  console.log(num1parsed);
+  num1parsed = parseFloat(num1);
   operation = "add";
   document.getElementById('display').innerHTML = '';
+  document.getElementById('decimalButton').disabled = false;
 }
 document.getElementById('plusButton').addEventListener('click', add);
 
 function subtract()
 {
   num1 = document.getElementById('display').textContent;
-  num1parsed = parseInt(num1);
-  console.log(num1parsed);
+  num1parsed = parseFloat(num1);
   operation = "subtract";
   document.getElementById('display').innerHTML = '';
+  document.getElementById('decimalButton').disabled = false;
 }
 document.getElementById('minusButton').addEventListener('click', subtract);
 
 function multiply()
 {
   num1 = document.getElementById('display').textContent;
-  num1parsed = parseInt(num1);
-  console.log(num1parsed);
+  num1parsed = parseFloat(num1);
   operation = "multiply";
   document.getElementById('display').innerHTML = '';
+  document.getElementById('decimalButton').disabled = true;
 }
 document.getElementById('multiplyButton').addEventListener('click', multiply);
 
 function divide()
 {
   num1 = document.getElementById('display').textContent;
-  num1parsed = parseInt(num1);
-  console.log(num1parsed);
+  num1parsed = parseFloat(num1);
   operation = "divide";
   document.getElementById('display').innerHTML = '';
+  document.getElementById('decimalButton').disabled = true;
 }
 document.getElementById('divideButton').addEventListener('click', divide);
 
+function percent() //this needs to be checked
+{
+  num1 = document.getElementById('display').textContent;
+  num1parsed = parseFloat(num1);
+  operation = "percent";
+}
+document.getElementById('percentButton').addEventListener('click', percent);
 
+
+
+//-------------------------------------------------
 
 function operate(operation, num1, num2)
 {
   switch(operation) {
     case "add": return num1 + num2;
+    case "percent": return num1 / 100; //this needs to be checked
     case "subtract": return num1 - num2;
     case "multiply": return num1 * num2;
-    case "divide": if (num1 / num2 === Infinity || num1 / num2 === -Infinity || num1 === 0 && num2 === 0)
+    case "divide": if(num1 / num2 === Infinity || num1 / num2 === -Infinity || num1 === 0 && num2 === 0)
       {
         return errorMessage;
       } 
@@ -68,13 +80,12 @@ const equalButton = document.querySelector('#equalButton');
 equalButton.addEventListener('click', () =>
 {
   num2 = document.getElementById('display').textContent;
-  num2parsed = parseInt(num2);
-  console.log(num2parsed);
-  document.getElementById('display').innerHTML = operate(operation, num1parsed, num2parsed);
-  console.log(num1parsed + num2parsed);
+  num2parsed = parseFloat(num2);
+  document.getElementById('display').innerHTML = Math.round(operate(operation, num1parsed, num2parsed) * 100) / 100;
+  document.getElementById('decimalButton').disabled = true;
 });
 
-  //clear function --------------------------------------------------------
+  //--------------------------------------------------------
   function allClear()
   {
     document.getElementById('display').innerHTML = '';
@@ -84,6 +95,20 @@ equalButton.addEventListener('click', () =>
   clearButton.addEventListener('click', () =>
   {
     allClear();
+    document.getElementById('decimalButton').disabled = false;
+  });
+
+//-------------------------------------------------------------
+  function decimalButton()
+  {
+   document.getElementById('display').textContent += '.';
+  }
+
+  let decimal = document.getElementById('decimalButton');
+  decimal.addEventListener('click', () =>
+  {
+    decimalButton();
+    document.getElementById('decimalButton').disabled = true;
   });
 
  //function for all digit values------------------------------------
@@ -97,3 +122,5 @@ for(let i = 0; i < digitBtn.length; i++)
     document.getElementById('display').textContent += digitBtn[i].textContent;
   })
 }
+
+
